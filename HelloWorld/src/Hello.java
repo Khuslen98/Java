0110138b89ce57d1c8e9024baee59bcde8194d3c
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-09-15 13:15:47 +0800

    Uuganaa ahiin zaavar

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void  main(String[] args) {
         System.out.println("Hello java");
         boolean isAlien = false;
-        if (!isAlien){
+        if (!isAlien) {
             System.out.println(false);
             System.out.println("2nd false");
         }
@@ -18,6 +18,16 @@ public class Hello {
         }
         if ((topScore > 90) || (secondScore <= 90)) {
             System.out.println("Either or both of the conditions are true");
+        }
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is error");
+        }
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen");
+        } else if (!isCar) {
+            System.out.println("jsajjsajajs");
         }
     }
 }
