d7b5c3b856812fb1ca26af2e5ae7ccb4ceeae406
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-09-15 12:42:35 +0800

    if then statement

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -6,6 +6,18 @@ public class Hello {
             System.out.println(false);
             System.out.println("2nd false");
         }
-
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("Your got high score");
+        }
+        int secondScore = 95;
+        System.out.println(topScore);
+        System.out.println(secondScore);
+        if ((topScore > secondScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+        if ((topScore > 90) || (secondScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
