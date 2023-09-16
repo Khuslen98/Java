4297f8821ae85b0039182735e910d3737448f1cc
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-09-16 21:23:21 +0800

    challenge done

---
+++ HelloWorld/src/Challenge_1.java
@@ -0,0 +1,16 @@
+public class Challenge_1 {
+
+    public static void  main(String[] args) {
+        double twenty = 20.00d;
+        double eigthy = 80.00d;
+        double hundred = (twenty + eigthy) * 100.00d;
+        System.out.println("hundred  " + hundred);
+        double remainder = hundred % 40.00d;
+        System.out.println("remainder " + remainder);
+        boolean isNoRemainder = (remainder == 0) ? true : false;
+        System.out.println(isNoRemainder);
+        if (isNoRemainder) {
+            System.out.println("Got some reminder");
+        }
+    }
+}
