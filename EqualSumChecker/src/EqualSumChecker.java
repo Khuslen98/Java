24fcf031f4dd971cc5628683e3ccd7641467ac58
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-09-24 19:01:01 +0800

    EqualSum checker done

---
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,12 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int param1, int param2, int param3) {
+
+        return param1 + param2 == param3;
+    }
+    public static void main(String[] args) {
+        int param1 = 1;
+        int param2 = 1;
+        int param3 = 2;
+        hasEqualSum(param1, param2, param3);
+    }
+}
