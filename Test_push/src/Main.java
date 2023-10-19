fed330d63c8367236a8b1fb07014e7473d9e2302
Khuslen.T (Хүслэн.Т) <khuslen.t@khanbank.com>
2023-10-19 17:37:59 +0800

    adding new push

---
+++ Test_push/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] atgs) {
+        int myVariable = 50;
+
+        myVariable++;
+//        myVariable--;
+        System.out.println("this is test " +
+                "another " +
+                "still more " +
+                myVariable);
+        int anotherVariable = 50;
+        anotherVariable--;
+        anotherVariable--;
+        anotherVariable--;
+        System.out.println("myVariable " + anotherVariable);
+    }
+}
