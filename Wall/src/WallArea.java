ad17b8a4991b0fc607bb878677090f9a2884d127
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-10-21 21:19:18 +0800

    wall done

---
+++ Wall/src/WallArea.java
@@ -0,0 +1,8 @@
+public class WallArea {
+    public static void main(String[] args){
+        Wall wall = new Wall(1.123,-1.0);
+        System.out.println("width= " + wall.getWidth());
+
+
+    }
+}
