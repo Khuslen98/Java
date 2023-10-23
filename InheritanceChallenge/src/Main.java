06f1a2fa3a8358f29683f0d9f8ab3fd80ca143a9
khuslen98 <khuslenu.tamiraa@gmail.com>
2023-10-23 13:33:18 +0800

    inheritance challenge done

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -6,9 +6,19 @@ public class Main {
         System.out.println("Age = " + Khu.getAge());
         System.out.println("Pay = " + Khu.collectPay());
 
-        Employee Joe = new Employee("Joe", "11/11/2312", "23/12/2023");
+        SalariedEmployee Joe = new SalariedEmployee("Joe", "11/11/2312", "23/12/2023", 35000);
         System.out.println(Joe);
         System.out.println("Age = " + Joe.getAge());
         System.out.println("Pay = " + Joe.collectPay());
+
+        System.out.println("Joe's paycheck = $ " + Joe + Joe.collectPay());
+
+        Joe.retire();
+        System.out.println("Joe's pension check = $ " + Joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1940", "09/09/2021", 15 );
+        System.out.println(mary);
+        System.out.println("Mary's paycheck = $ " + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $ " + mary.getDoublePay());
     }
 }
