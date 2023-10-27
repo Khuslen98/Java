8cc02563cd387cd1ac9270210611bb780689d0f4
Khuslen98 <khuslenu.tamiraa@gmail.com>
2023-10-27 12:51:05 +0800

    commiting encapsulation

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        EnhancedPlayer khu = new EnhancedPlayer("Khu");
+        System.out.println("Initial health is = " + khu.healthRemaining());
+//        Player player = new Player();
+//        player.name = "KHUSLEN";
+//        player.health = 20;
+//        player.weapon = "ARROW";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remain First hit = " + player.healthRemaining());
+//
+//        player.health = 200;
+//
+//        player.loseHealth(11);
+//        System.out.println("Remain Second hit = " + player.healthRemaining());
+    }
+}
