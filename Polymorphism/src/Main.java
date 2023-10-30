245247cdf150e90c9de6b296934b630243048ee2
Khuslen98 <khuslenu.tamiraa@gmail.com>
2023-10-30 10:32:47 +0800

    commit added not done

---
+++ Polymorphism/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Movie theMovie = Movie.getMovie("c", "Harry Potter" );
+//        theMovie.watchMovie();
+        Scanner s = new Scanner(System.in);
+    }
+}
