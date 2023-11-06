eada22754406c75900c4117ca126fde1cb835550
Khuslen98 <khuslenu.tamiraa@gmail.com>
2023-11-06 13:32:45 +0800

    Coding exercise 42, Array challenge

--- MinimumElementChallengeg/src/dev/lpa/Main.java
+++ MinimumElementChallengeg/src/dev/lpa/Main.java
@@ -6,27 +6,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        readIntegers(6);
-        int[] returnedArray = readIntegers();
-        System.out.println(Arrays.toString(returnedArray));
+        System.out.println(Arrays.toString(readElements(5)));
 
-        int returnedMin = findMin(returnedArray);
-        System.out.println("min = " + returnedMin);
     }
-    private static int[] readIntegers() {
+    private static int readInteger() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter " +  " integer values:\r");
-        String input = scanner.nextLine();
-
-        String[] splits = input.split(",");
-        int[] values = new int[splits.length];
-
-        for (int i = 0; i <splits.length; i++) {
-            values[i] = Integer.parseInt(splits[i].trim());
-        }
-
-        return values;
+        System.out.println("Enter the number of elements: ");
+        int elements = scanner.nextInt();
+        return elements;
     }
-    public static int findMin(int[] array) {
+
+    private static int[] readElements(int count) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            array[i] = number;
+        }
+        return array;
+    }
+    private static int findMin(int[] array) {
         int min = Integer.MAX_VALUE;
         for (int el: array) {
             if (el < min) {
