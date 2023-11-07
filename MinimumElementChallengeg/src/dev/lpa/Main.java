e1a1656f159679b44844cafb10126377f0915845
Khuslen98 <khuslenu.tamiraa@gmail.com>
2023-11-07 10:38:36 +0800

    Coding exercise 43: Reverse Array challenge

--- MinimumElementChallengeg/src/dev/lpa/Main.java
+++ MinimumElementChallengeg/src/dev/lpa/Main.java
@@ -49,5 +49,16 @@ public class Main {
         }
         return maxIndex;
     }
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+        for (int i = 0; i < halfLength; i++){
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
 
 }
