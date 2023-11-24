238384708bc36d9b1a8d812c907d1f239fe75938
Khuslen98 <khuslenu.tamiraa@gmail.com>
2023-11-24 13:58:45 +0800

    adding skeleton of Bank challenge

--- AutoBoxingChallenge/src/dev/lpa/Mainn.java
+++ AutoBoxingChallenge/src/dev/lpa/Mainn.java
@@ -9,6 +9,9 @@ record Customer(String name, ArrayList<Double> transactions) {
                 new ArrayList<Double>(500));
         transactions.add(initialDeposit);
     }
+
+
+
 }
 public class Mainn {
     public static void main(String[] args) {
